49a257b6dce0587938834495197b3d00131db9f1
Shashi5657 <sangepushashidhar@gmail.com>
2024-05-19 15:48:19 +0530

    basics are finished

--- 00-js-basics/assets/scripts/app.js
+++ 00-js-basics/assets/scripts/app.js
@@ -1,34 +1,61 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry); //pushes value to the defined array
+  console.log(logEntries); //prints 1st value of the array
+}
 
 function add() {
   //parseInt converts string to number, alternative way is using +
   //such as currentResult + +userInput.value
   const enteredValue = parseInt(userInput.value);
   const calcDescription = `${currentResult} + ${enteredValue}`;
+  const initialResult = currentResult;
   currentResult = currentResult + enteredValue;
   outputResult(currentResult, calcDescription);
+  writeToLog("ADD", initialResult, enteredValue, currentResult);
+
+//   console.log(logEntry.number); //accessing object values
 }
 
 function subtract() {
   const enteredValue = parseInt(userInput.value);
   const calcDescription = `${currentResult} - ${enteredValue}`;
+  const initialResult = currentResult;
   currentResult -= enteredValue;
   outputResult(currentResult, calcDescription);
+  writeToLog("SUBTRACT", initialResult, enteredValue, currentResult);
 }
 
 function multiply() {
   const enteredValue = parseInt(userInput.value);
   const calcDescription = `${currentResult} * ${enteredValue}`;
+  const initialResult = currentResult;
   currentResult *= enteredValue;
   outputResult(currentResult, calcDescription);
+  writeToLog("MULTIPLY", initialResult, enteredValue, currentResult);
 }
 
 function divide() {
   const enteredValue = parseInt(userInput.value);
   const calcDescription = `${currentResult} / ${enteredValue}`;
+  const initialResult = currentResult;
   currentResult = currentResult / enteredValue;
   outputResult(currentResult, calcDescription);
+  writeToLog("DIVIDE", initialResult, enteredValue, currentResult);
 }
 
 addBtn.addEventListener("click", add);
