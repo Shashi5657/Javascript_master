c168b9011b31db9b377ee1ad384d7b9108121c84
Shashi5657 <sangepushashidhar@gmail.com>
2024-05-20 23:16:19 +0530

    conditional code execution

---
+++ 01-monster-killer-project/assets/scripts/app.js
@@ -0,0 +1,75 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You Won");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You Lost");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("You have a draw!");
+  }
+}
+
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound()
+}
+
+function attackHandler() {
+  //   const damage = dealMonsterDamage(ATTACK_VALUE);
+  //   currentMonsterHealth -= damage;
+  //   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  //   currentPlayerHealth -= playerDamage;
+  //   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+  //     alert("You Won");
+  //   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+  //     alert("You Lost");
+  //   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+  //     alert("You have a draw!");
+  //   }
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  //   const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  //   currentMonsterHealth -= damage;
+  //   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  //   currentPlayerHealth -= playerDamage;
+  //   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+  //     alert("You Won");
+  //   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+  //     alert("You Lost");
+  //   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+  //     alert("You have a draw!");
+  //   }
+  attackMonster("STRONG_ATTACK");
+}
+
+function healPlayerHandler() {
+    let healValue;
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth += HEAL_VALUE;
+  endRound()
+}
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
