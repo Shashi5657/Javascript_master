c58fe944c4d27dda4da39accf2c12bb35448f3be
Shashi5657 <sangepushashidhar@gmail.com>
2024-05-20 14:46:42 +0530

    conditional code execution

--- 00-js-basics/assets/scripts/app.js
+++ 00-js-basics/assets/scripts/app.js
@@ -2,6 +2,11 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
 function writeToLog(
   operationIdentifier,
   prevResult,
@@ -18,44 +23,68 @@ function writeToLog(
   console.log(logEntries); //prints 1st value of the array
 }
 
+function calculateResult(calculationType) {
+  const enteredValue = parseInt(userInput.value);
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === "ADD") {
+    currentResult = currentResult + enteredValue;
+    mathOperator = "+";
+  } else if(calculationType === 'SUBTRACT') {
+    currentResult -= enteredValue;
+    mathOperator = "-";
+  } else if(calculationType === 'MULTIPLY'){
+    currentResult *= enteredValue;
+    mathOperator = "*";
+  } else if(calculationType === 'DIVIDE') {
+    currentResult /= enteredValue;
+    mathOperator = '/'
+  }
+  createAndWriteOutput(mathOperator, enteredValue, initialResult);
+  writeToLog(calculationType, initialResult, enteredValue, currentResult);
+}
+
 function add() {
   //parseInt converts string to number, alternative way is using +
   //such as currentResult + +userInput.value
-  const enteredValue = parseInt(userInput.value);
-  const calcDescription = `${currentResult} + ${enteredValue}`;
-  const initialResult = currentResult;
-  currentResult = currentResult + enteredValue;
-  outputResult(currentResult, calcDescription);
-  writeToLog("ADD", initialResult, enteredValue, currentResult);
+  // const enteredValue = parseInt(userInput.value);
+  // const initialResult = currentResult;
+  // currentResult = currentResult + enteredValue;
+  // createAndWriteOutput('+', enteredValue, initialResult)
+  // writeToLog("ADD", initialResult, enteredValue, currentResult);
+  calculateResult("ADD");
 
-//   console.log(logEntry.number); //accessing object values
+  //   console.log(logEntry.number); //accessing object values
 }
 
 function subtract() {
-  const enteredValue = parseInt(userInput.value);
-  const calcDescription = `${currentResult} - ${enteredValue}`;
-  const initialResult = currentResult;
-  currentResult -= enteredValue;
-  outputResult(currentResult, calcDescription);
-  writeToLog("SUBTRACT", initialResult, enteredValue, currentResult);
+  // const enteredValue = parseInt(userInput.value);
+  // const calcDescription = `${currentResult} - ${enteredValue}`;
+  // const initialResult = currentResult;
+  // currentResult -= enteredValue;
+  // outputResult(currentResult, calcDescription);
+  // writeToLog("SUBTRACT", initialResult, enteredValue, currentResult);
+  calculateResult("SUBTRACT");
 }
 
 function multiply() {
-  const enteredValue = parseInt(userInput.value);
-  const calcDescription = `${currentResult} * ${enteredValue}`;
-  const initialResult = currentResult;
-  currentResult *= enteredValue;
-  outputResult(currentResult, calcDescription);
-  writeToLog("MULTIPLY", initialResult, enteredValue, currentResult);
+  // const enteredValue = parseInt(userInput.value);
+  // const calcDescription = `${currentResult} * ${enteredValue}`;
+  // const initialResult = currentResult;
+  // currentResult *= enteredValue;
+  // outputResult(currentResult, calcDescription);
+  // writeToLog("MULTIPLY", initialResult, enteredValue, currentResult);
+  calculateResult('MULTIPLY')
 }
 
 function divide() {
-  const enteredValue = parseInt(userInput.value);
-  const calcDescription = `${currentResult} / ${enteredValue}`;
-  const initialResult = currentResult;
-  currentResult = currentResult / enteredValue;
-  outputResult(currentResult, calcDescription);
-  writeToLog("DIVIDE", initialResult, enteredValue, currentResult);
+  // const enteredValue = parseInt(userInput.value);
+  // const calcDescription = `${currentResult} / ${enteredValue}`;
+  // const initialResult = currentResult;
+  // currentResult = currentResult / enteredValue;
+  // outputResult(currentResult, calcDescription);
+  // writeToLog("DIVIDE", initialResult, enteredValue, currentResult);
+  calculateResult('DIVIDE')
 }
 
 addBtn.addEventListener("click", add);
