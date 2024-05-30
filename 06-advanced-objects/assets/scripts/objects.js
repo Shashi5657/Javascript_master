b8903d3cdb25f4d45511acf64ad23632f0c50c84
Shashi5657 <sangepushashidhar@gmail.com>
2024-05-30 11:51:04 +0530

    starting module on advanced objects

--- 06-advanced-objects/assets/scripts/objects.js
+++ 06-advanced-objects/assets/scripts/objects.js
@@ -1,10 +1,21 @@
 const person = {
     name : 'Shashi',
-    age : 23,
+    age : 22,
     hobbies : ['Cooking', 'Coding'],
     greet : function(){
         alert('Hii there!')
     }
 }
+
+//to add a new property or to modify a prop in object
+
+person.isAdmin = true;
+person.age = 23
+
+//to delete a prop in obj
+
+delete person.age
+
+console.log(person)
 
 person.greet();
