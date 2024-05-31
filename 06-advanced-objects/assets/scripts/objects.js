8362de5a7cf816008ca73c05731c43297e9a01a2
Shashi5657 <sangepushashidhar@gmail.com>
2024-05-31 12:26:58 +0530

    starting module on advanced objects

--- 06-advanced-objects/assets/scripts/objects.js
+++ 06-advanced-objects/assets/scripts/objects.js
@@ -1,21 +1,36 @@
+//other example of using obj keys in array like ways
+
+const movieList = document.getElementById('movie-list')
+
+movieList.style['display'] = 'block'
+movieList.style['backgroundColor'] = 'red'
+
+
 const person = {
-    name : 'Shashi',
-    age : 22,
-    hobbies : ['Cooking', 'Coding'],
-    greet : function(){
-        alert('Hii there!')
-    }
-}
+  //we can also use key name like this, if we want to use any other symbols like -
+  //we should wrap the key in single quotes
+  "first-name": "Shashi",
+  // name : 'Shashi',
+  age: 22,
+  hobbies: ["Cooking", "Coding"],
+  greet: function () {
+    alert("Hii there!");
+  },
+};
+
+//now to access that kind of keys, we know obj are arrays
+
+console.log(person['first-name'])
 
 //to add a new property or to modify a prop in object
 
 person.isAdmin = true;
-person.age = 23
+person.age = 23;
 
 //to delete a prop in obj
 
-delete person.age
+delete person.age;
 
-console.log(person)
+console.log(person);
 
 person.greet();
